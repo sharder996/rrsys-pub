fd9280f40f301774cba96c06954f5e39ddfcf1a4
Kuro <sharder996@gmail.com>
2020-06-18 12:57:45 -0500

    Initial commit

---
+++ app/src/main/java/comp3350/rrsys/MainActivity.java
@@ -0,0 +1,16 @@
+package comp3350.rrsys;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Thus begins the story of Group 8's fantasic adventure into the world of COMP 3350
+    }
+}
