81b8ab4dbafebdec2ffb18d19b56196c091d4d5f
Kuro <sharder996@gmail.com>
2020-07-09 11:31:29 -0500

    test cleanliness changes

--- app/src/test/java/comp3350/rrsys/tests/objects/TestCustomer.java
+++ app/src/test/java/comp3350/rrsys/tests/objects/TestCustomer.java
@@ -2,117 +2,155 @@ package comp3350.rrsys.tests.objects;
 
 import junit.framework.TestCase;
 
-import org.junit.Test;
-
 import comp3350.rrsys.objects.Customer;
-/* TODO:
- *      - Add more test cases for invalid data
- */
+
+import static org.junit.Assert.assertNotEquals;
+
 public class TestCustomer extends TestCase
 {
     public TestCustomer(String arg0) { super(arg0); }
 
-    public void testCustomer()
+    public void testCustomerCreation()
     {
-        Customer customer0;
+        System.out.println("\nStarting testCustomerCreation");
 
-        System.out.println("\nStarting TestCustomer");
+        Customer customer = new Customer("Ivan", "Barbashev", "204-555-9999");
 
-        customer0 = new Customer("Ivan", "Barbashev", "204-555-9999");
-        assertNotNull(customer0);
-        assertTrue("Ivan".equals(customer0.getFirstName()));
-        assertTrue("Barbashev".equals(customer0.getLastName()));
-        assertTrue("Ivan Barbashev".equals((customer0.getFullName())));
-        assertFalse("204-555-9999".equals(Integer.toString(customer0.getPhoneNumber())));
-        assertEquals(2045559999, customer0.getPhoneNumber());
+        assertNotNull(customer);
+        assertEquals("Ivan", customer.getFirstName());
+        assertEquals("Barbashev", customer.getLastName());
+        assertEquals("Ivan Barbashev", customer.getFullName());
+        assertNotEquals("204-555-9999", Integer.toString(customer.getPhoneNumber()));
+        assertEquals(2045559999, customer.getPhoneNumber());
 
-        customer0.setFirstName("Annie");
-        customer0.setLastName("Apple");
-        customer0.setPhoneNumber("204-555-1111");
-        assertNotNull(customer0);
-        assertTrue("Annie".equals(customer0.getFirstName()));
-        assertTrue("Apple".equals(customer0.getLastName()));
-        assertTrue("Annie Apple".equals((customer0.getFullName())));
-        assertEquals(2045551111, customer0.getPhoneNumber());
+        customer.setFirstName("Annie");
+        customer.setLastName("Apple");
+        customer.setPhoneNumber("204-555-1111");
 
+        assertNotNull(customer);
+        assertEquals("Annie", customer.getFirstName());
+        assertEquals("Apple", customer.getLastName());
+        assertEquals("Annie Apple", customer.getFullName());
+        assertEquals(2045551111, customer.getPhoneNumber());
+
+        System.out.println("\nEnding testCustomerCreation");
+    }
+
+    public void testCustomerNotEquals()
+    {
+        System.out.println("\nStarting testCustomerNotEquals");
+
+        Customer customer0 = new Customer("Ivan", "Barbashev", "204-555-9999");
         Customer customer1 = new Customer("Judy", "Test", "204-414-0198");
+        Customer customer2 = new Customer("Judy", "Test", "204-414-0198");
+
         assertNotNull(customer1);
         assertFalse(customer0.equals(customer1));
 
-        Customer customer2 = new Customer("Judy", "Test", "204-414-0198");
         assertNotNull(customer2);
         assertFalse(customer1.equals(customer2));
 
-        Customer customer3 = null;
+        System.out.println("\nEnding testCustomerNotEquals");
+    }
+
+    public void testCustomerEmptyParameters()
+    {
+        System.out.println("\nStarting testCustomerEmptyParameters");
+
+        Customer customer = null;
         try
         {
-            customer3 = new Customer("", "Test", "123-456-7890");
+            customer = new Customer("", "Test", "123-456-7890");
+            fail();
         }
         catch (IllegalArgumentException e)
         {
-            e.getMessage();
+            assertNull(customer);
         }
-        assertNull(customer3);
 
         try
         {
-            customer3 = new Customer("Test", "", "123-456-7890");
+            customer = new Customer("Test", "", "123-456-7890");
+            fail();
         }
         catch (IllegalArgumentException e)
         {
-            e.getMessage();
+            assertNull(customer);
         }
-        assertNull(customer3);
 
         try
         {
-            customer3 = new Customer("Test", "Testing", "");
+            customer = new Customer("Test", "Testing", "");
+            fail();
         }
         catch (IllegalArgumentException e)
         {
-            e.getMessage();
+            assertNull(customer);
         }
-        assertNull(customer3);
+
+        System.out.println("\nEnding testCustomerEmptyParameters");
+    }
+
+    public void testCustomerAlphanumericPhoneNumber()
+    {
+        System.out.println("\nStarting testCustomerAlphanumericPhoneNumber");
+
+        Customer customer = null;
+        try
+        {
+            customer = new Customer("Test", "Testing", "aaa-456-7890");
+            fail();
+        }
+        catch (IllegalArgumentException e)
+        {
+            assertNull(customer);
+        }
+
+        System.out.println("\nEnding testCustomerAlphanumericPhoneNumber");
+    }
+
+    public void testCustomerAlphanumericName()
+    {
+        System.out.println("\nStarting testCustomerAlphanumericName");
+
+        Customer customer = null;
+        try
+        {
+            customer = new Customer("Test3", "Testing", "123-456-7890");
+            fail();
+        }
+        catch (IllegalArgumentException e)
+        {
+            assertNull(customer);
+        }
 
         try
         {
-            customer3 = new Customer("Test", "Testing", "aaa-456-7890");
+            customer = new Customer("Test", "Testing6", "123-456-7890");
+            fail();
         }
         catch (IllegalArgumentException e)
         {
-            e.getMessage();
+            assertNull(customer);
         }
-        assertNull(customer3);
 
+        System.out.println("\nEnding testCustomerAlphanumericName");
+    }
+
+    public void testCustomerInvalidPhoneNumberLength()
+    {
+        System.out.println("\nStarting testCustomerInvalidPhoneNumberLength");
+
+        Customer customer = null;
         try
         {
-            customer3 = new Customer("Test3", "Testing", "123-456-7890");
+            customer = new Customer("Test", "Testing", "456-7890");
+            fail();
         }
         catch (IllegalArgumentException e)
         {
-            e.getMessage();
+            assertNull(customer);
         }
-        assertNull(customer3);
-
-        try
-        {
-            customer3 = new Customer("Test", "Testing6", "123-456-7890");
-        }
-        catch (IllegalArgumentException e)
-        {
-            e.getMessage();
-        }
-        assertNull(customer3);
-
-        try
-        {
-            customer3 = new Customer("Test", "Testing", "456-7890");
-        }
-        catch (IllegalArgumentException e)
-        {
-            e.getMessage();
-        }
-        assertNull(customer3);
-        System.out.println("\nEnd TestCustomer");
+        System.out.println("\nEnding testCustomerInvalidPhoneNumberLength");
     }
 }
