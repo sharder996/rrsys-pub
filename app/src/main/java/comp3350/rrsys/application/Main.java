d68ddd28af0e54850b1b933d02db8f1a58c731e8
Louis <louis.manahan@hotmail.com>
2020-07-19 21:48:42 -0500

    fixed SC.script with tables & relations

--- app/src/main/java/comp3350/rrsys/application/Main.java
+++ app/src/main/java/comp3350/rrsys/application/Main.java
@@ -3,6 +3,7 @@ package comp3350.rrsys.application;
 public class Main
 {
     public static final String dbName = "RR";
+    private static String dbPathName = "database/SC";
 
     public static void main(String[] args)
     {
@@ -21,5 +22,10 @@ public class Main
     public static void shutDown()
     {
         Services.closeDataAccess();
+    }
+
+    public static void setDBPathName(String pathName) {
+        System.out.println("Setting DB path to: " + pathName);
+        dbPathName = pathName;
     }
 }
--- app/src/main/java/comp3350/rrsys/presentation/HomeActivity.java
+++ app/src/main/java/comp3350/rrsys/presentation/HomeActivity.java
@@ -1,11 +1,18 @@
 package comp3350.rrsys.presentation;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.FileWriter;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.content.Intent;
+import android.content.Context;
+import android.content.res.AssetManager;
 import comp3350.rrsys.R;
 import comp3350.rrsys.application.Main;
 
@@ -59,5 +66,57 @@ public class HomeActivity extends Activity
     {
         Intent reviewReservationIntent = new Intent(HomeActivity.this, GetReviewReservationActivity.class);
         HomeActivity.this.startActivity(reviewReservationIntent);
+    }
+
+    private void copyDatabaseToDevice() {
+        final String DB_PATH = "db";
+
+        String[] assetNames;
+        Context context = getApplicationContext();
+        File dataDirectory = context.getDir(DB_PATH, Context.MODE_PRIVATE);
+        AssetManager assetManager = getAssets();
+
+        try {
+
+            assetNames = assetManager.list(DB_PATH);
+            for (int i = 0; i < assetNames.length; i++) {
+                assetNames[i] = DB_PATH + "/" + assetNames[i];
+            }
+
+            copyAssetsToDirectory(assetNames, dataDirectory);
+
+            Main.setDBPathName(dataDirectory.toString() + "/" + Main.dbName);
+
+        } catch (IOException ioe) {
+            Messages.warning(this, "Unable to access application data: " + ioe.getMessage());
+        }
+
+    }
+
+    public void copyAssetsToDirectory(String[] assets, File directory) throws IOException {
+        AssetManager assetManager = getAssets();
+
+        for (String asset : assets) {
+            String[] components = asset.split("/");
+            String copyPath = directory.toString() + "/" + components[components.length - 1];
+            char[] buffer = new char[1024];
+            int count;
+
+            File outFile = new File(copyPath);
+
+            if (!outFile.exists()) {
+                InputStreamReader in = new InputStreamReader(assetManager.open(asset));
+                FileWriter out = new FileWriter(outFile);
+
+                count = in.read(buffer);
+                while (count != -1) {
+                    out.write(buffer, 0, count);
+                    count = in.read(buffer);
+                }
+
+                out.close();
+                in.close();
+            }
+        }
     }
 }
