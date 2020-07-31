5483a235993761c3c2c0c0ea94205752cf7b65fd
Yan511 <yanwen970511@gmail.com>
2020-07-30 20:24:18 -0500

    Comment suggestReservation

--- app/src/main/java/comp3350/rrsys/business/AccessReservations.java
+++ app/src/main/java/comp3350/rrsys/business/AccessReservations.java
@@ -34,13 +34,14 @@ public class AccessReservations
     public String deleteReservation(int reservationID) { return dataAccess.deleteReservation(reservationID); }
 
     // return an array of suggested reservations in order
-    // which has the same "length" as (endTime-startTime)
+    // which has the same "length" as (endTime-startTime), which is rounded to integer multiple of 15 minutes (an inteval)
+    // and start within 30 minutes (2 intervals) of the startTime (rouding to closest interval)
     public ArrayList<Reservation> suggestReservations(DateTime startTime, DateTime endTime, int numPeople)
     {
         ArrayList<Reservation> results = new ArrayList<>();
-        int index = getIndex(startTime);
-        int totalIncrement = (startTime.getPeriod(endTime)+7)/15; // total num of increments
-        int maxIndex = Table.INTERVALS_PER_DAY; // max index
+        int index = getIndex(startTime); // the index of interval in the business hours of a day for start time (after rouding)
+        int totalIncrement = (startTime.getPeriod(endTime)+7)/15; // total num of increments (+7 for rounding to the closest number of intervals)
+        int maxIndex = Table.INTERVALS_PER_DAY; // max index (the number of 15-minutes intervals in the business hours of a day)
         ArrayList<Table> tables = new ArrayList<>();
         dataAccess.getTableSequential(tables);
 
@@ -48,18 +49,27 @@ public class AccessReservations
         for(int t = 0; t < tables.size(); t++)
         {
             table = tables.get(t);
+            // check if the table can hold numPeople
             if(table.getCapacity() >= numPeople)
             {
+                // the available array of table with tableID t+1 at the day defined by (year, month, date) of input startTime
+                // available.length == Table.INTERVALS_PER_DAY, each index represents a 15-minutes interval
+                // each boolean represents whether the table is available in the corresponding 15-minutes interval
                 boolean[] available = dataAccess.getAvailable(t+1, startTime);
-                // within +- half hour of the start time
+
+                // within +- 2 intervals (half hour) of the start time
                 int i = Math.max(index-2, 0);
                 while(i <= index + 2 && i < maxIndex)
                 {
+                    // obtain the first available index/interval of the table
                     while(i <= index + 2 && i < maxIndex && !available[i])
                         i++;
+
+                    // check if the first available index is within +- half hours of the index of startTime
                     if(i <= index + 2 && i < maxIndex)
                     {
-                        int numIncrement = 1;
+                        int numIncrement = 1; // the number of consecutive available intervals
+                        // count the number of consecutive available intervals
                         for(int time = i + 1; time < i + totalIncrement; time++)
                         {
                             if(time < maxIndex && available[time])
@@ -67,14 +77,24 @@ public class AccessReservations
                             else
                                 break;
                         }
+
+                        // check if numIncrement (length of consecutive available intervals) == totalIncrement (length of endTime-startTime)
+                        // if so, i will be the index of startTime of suggested reservation
+                        // i+numIncrement will be the index of endTime of suggested reservation since it has the same length
                         if(numIncrement == totalIncrement)
                         {
-                            DateTime start = dataAccess.getDateTime(startTime, i);
-                            DateTime end = dataAccess.getDateTime(endTime, i+numIncrement);
+                            // obtain the DateTime which has same (year, month, day) as startTime, and (hour, minute) corresponding to index i
+                            DateTime start = dataAccess.getDateTime(startTime, i); // startTime of suggested reservation
+                            DateTime end = dataAccess.getDateTime(endTime, i+numIncrement); // endTime of suggested reservation
+
+                            // ordered insert the reservation to results
+                            //ordered first by how close to the input startTime
+                            // if same, ordered secondly by how close the table capacity to number of people
+                            // if still same, ordered thirdly by table ID in ascending order
                             dataAccess.orderedInsert(results, new Reservation(table.getTID(), numPeople, start, end), startTime);
                         }
                     }
-                    i++;
+                    i++; // check for next index i
                 }
             }
         }
@@ -82,6 +102,6 @@ public class AccessReservations
     }
 
     // return the index corresponding to a date time
-    // the closest index of the 15-minutes increment in the business hour of a day
+    // the closest index of the 15-minutes increment/interval in the business hour of a day
     public static int getIndex(DateTime time) { return (time.getHour()-Table.START_TIME)*4 + (time.getMinutes()+7)/15; }
 }
